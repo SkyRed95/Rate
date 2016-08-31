e270eca053cf304c500d63bfac6ac4070ee2ad42
SkyRed <19skyred95@gmail.com>
2016-08-31 22:05:52 +0300

    get_rate

--- get_rate.php
+++ get_rate.php
@@ -3,7 +3,7 @@ $ccy = $_GET['ccy'];
 $range = $_GET['range'];
 $connect = new mysqli("localhost", "SkyRed", "1596357", "rate_db");
 
-$result = $connect->query("SELECT `Ccy`, `Base_ccy`, `Buy`, `Sale`, `Date` FROM `rate` WHERE `Ccy` LIKE '" . $ccy . "'");
+$result = $connect->query("SELECT `Ccy`, `Base_ccy`, `Buy`, `Sale`, `Date`FROM `rate` WHERE `Ccy` LIKE '" . $ccy . "'");
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
