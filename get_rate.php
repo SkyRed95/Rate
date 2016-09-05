d2299479039d7294be0da89eb85d39fce8179b76
SkyRed <19skyred95@gmail.com>
2016-09-05 23:26:10 +0300

    rate_reformat

--- get_rate.php
+++ get_rate.php
@@ -6,23 +6,23 @@
         <th>Курс продажи</th>
         <th>Дата</th>
     </tr>
-<?php
-$ccy = $_GET['ccy'];
-$range = $_GET['range'];
-$connect = new mysqli("localhost", "SkyRed", "1596357", "rate_db");
+    <?php
+    $ccy = $_GET['ccy'];
+    $range = $_GET['range'];
+    $connect = new mysqli("localhost", "SkyRed", "1596357", "rate_db");
 
-$result = $connect->query("SELECT `Ccy`, `Base_ccy`, `Buy`, `Sale`, `Date` FROM `rate` WHERE `Ccy` LIKE '" . $ccy . "' and
+    $result = $connect->query("SELECT `Ccy`, `Base_ccy`, `Buy`, `Sale`, `Date` FROM `rate` WHERE `Ccy` LIKE '" . $ccy . "' and
 Date >= '" . $range[0] . "' and Date <= '" . $range[1] . "'");
-if ($result->num_rows > 0) {
+    if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         ?>
-            <tr>
-                <td><?php echo $row["Ccy"] ?></td>
-                <td><?php echo $row["Base_ccy"] ?></td>
-                <td><?php echo $row["Buy"] ?></td>
-                <td><?php echo $row["Sale"] ?></td>
-                <td><?php echo $row["Date"] ?></td>
-            </tr>
+        <tr>
+            <td><?php echo $row["Ccy"] ?></td>
+            <td><?php echo $row["Base_ccy"] ?></td>
+            <td><?php echo $row["Buy"] ?></td>
+            <td><?php echo $row["Sale"] ?></td>
+            <td><?php echo $row["Date"] ?></td>
+        </tr>
         <?php
     }
     ?>
