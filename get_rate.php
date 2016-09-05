d13ebbe17e679d23b5637168dd416a380ee43cb5
SkyRed <19skyred95@gmail.com>
2016-09-05 22:22:59 +0300

    table

--- get_rate.php
+++ get_rate.php
@@ -1,3 +1,11 @@
+<table border="1">
+    <tr>
+        <th>Код валюты</th>
+        <th>Код национальной валюты</th>
+        <th>Курс покупки</th>
+        <th>Курс продажи</th>
+        <th>Дата</th>
+    </tr>
 <?php
 $ccy = $_GET['ccy'];
 $range = $_GET['range'];
@@ -7,9 +15,20 @@ $result = $connect->query("SELECT `Ccy`, `Base_ccy`, `Buy`, `Sale`, `Date` FROM 
 Date >= '" . $range[0] . "' and Date <= '" . $range[1] . "'");
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo $row["Ccy"] . $row["Base_ccy"] . $row["Buy"] . $row["Sale"];
+        ?>
+            <tr>
+                <td><?php echo $row["Ccy"] ?></td>
+                <td><?php echo $row["Base_ccy"] ?></td>
+                <td><?php echo $row["Buy"] ?></td>
+                <td><?php echo $row["Sale"] ?></td>
+                <td><?php echo $row["Date"] ?></td>
+            </tr>
+        <?php
     }
+    ?>
+</table>
+<?php
 } else {
-    echo "0 results";
+    echo "Нет данных";
 }
 $connect->close();
