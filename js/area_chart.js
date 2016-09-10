ff005ce1922ec8aee5c155f8722635d341d67a7b
SkyRed <19skyred95@gmail.com>
2016-09-10 21:07:21 +0300

    chart

--- js/area_chart.js
+++ js/area_chart.js
@@ -6,23 +6,10 @@ function drawChart() {
     var base_ccy = $('.base_ccy_td').html();
     $('.tr').each(function (i) {
         var $tds = $(this).find('td');
-        arr_to_diagram[i] = [[$tds.eq(4).text()],
-            [$tds.eq(2).text()],
-            [$tds.eq(3).text()]];
+        arr_to_diagram[i] = [String([$tds.eq(4).html()]),
+            Number([$tds.eq(2).text()]),
+            Number([$tds.eq(3).text()])];
     })
-
-    // var data = new google.visualization.DataTable();
-    // data.addColumn('string', ccy);
-    // data.addColumn('string', base_ccy);
-    // data.addColumn('string', 'Покупка');
-    // data.addColumn('string', 'Продажа');
-    // data.addColumn('string', 'Дата');
-    // data.addRows(arr_to_diagram);
-// alert(arr_to_diagram[1]);
-//     var arr_to_diagram = [["2016-09-02", 6, 1],
-//         ["2016-09-04", 12, 2],
-//         ["2016-09-05", 18, 14]];
-
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Date');
     data.addColumn('number', ccy);
